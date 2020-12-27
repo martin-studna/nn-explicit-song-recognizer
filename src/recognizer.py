7d109dc9b4f3cd0a1b7c1c955aa4457ccbca5953
Jan Babušík <qwak623@gmail.com>
2020-12-27 12:53:03 +0100

    tensorflow_init

--- src/recognizer.py
+++ src/recognizer.py
@@ -1,18 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as tf
 from sklearn.datasets import make_classification
 from sklearn.preprocessing import StandardScaler
-
 
 def sigmoid(x):
   return 1/(1+np.exp(-x))
 
-
 # Load data
 data = pd.read_csv("../data/data.csv")
 print(data)
-
 
 # === PREPROCESSING ===
 
@@ -22,14 +20,38 @@ data = data.drop(['release_date'], axis=1)
 data = data.drop(['id'], axis=1)
 data = data.drop(['artists'], axis=1)
 
+print(data)
+
+batch_size = 100
+epochs = 10
 
 X = data.drop(['explicit'], axis=1).values
 Y = data['explicit'].values
 
 w = np.zeros(( ))
 
-print(X)
-print(Y)
+print(X[0])
+print(Y[0])
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(14, activation=tf.nn.relu, input_shape=(14,)),
+    tf.keras.layers.Dense(10, activation=tf.nn.relu),
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+model.compile(
+    optimizer="SGD",
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+
+tb_callback = tf.keras.callbacks.TensorBoard('true', update_freq=100, profile_batch=0)
+model.fit(
+    X, Y,
+    batch_size=batch_size, epochs=epochs,
+    callbacks=[tb_callback]
+)
+
 
 # scaler = StandardScaler()
 # scaler.fit(X)
